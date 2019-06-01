1cf7fafd2de516174863ba15e1961ed052a1ba5d
arturo <arty19972013@gmail.com>
2019-06-01 12:42:47 -0400

    cambios finales

--- ProyNew/resources/views/estudiante/inscripcion/sesionMateria/portafolio.blade.php
+++ ProyNew/resources/views/estudiante/inscripcion/sesionMateria/portafolio.blade.php
@@ -5,14 +5,14 @@
 
     <div class="list-group">
       @foreach($paquete as $p )
-        <a href="{{URL::action('estudiante\SesionMateriaController@descargar',
-                  array('id'=>$p->ID_PORTAFOLIO,'ruta'=>$p->RUTA_ARCHIVO))}}"
-                  class="list-group-item list-group-item-action">{{$p->RUTA_ARCHIVO}}</a>
+        <a href="{{URL::action('estudiante\SesionMateriaController@descargar',array('id'=>$p->ID_PORTAFOLIO,'ruta'=>$p->RUTA_ARCHIVO))}}" class="list-group-item list-group-item-action">{{$p->RUTA_ARCHIVO}}</a>
       @endforeach
       <div class="list-group-item list-group-item-action">
         @include('estudiante.inscripcion.sesionMateria.prueba')
       </div>
 
-
+      
 
 @endsection
+ 
+ 
